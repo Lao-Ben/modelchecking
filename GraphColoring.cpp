cd32a0f0526ae98c0c578598493ff3d13e7d1fe4
lefebv_f <benjamin.lefebvre@epita.fr>
2013-03-31 19:42:23 +0200

    * useless file removed

--- GraphColoring.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  GraphColoring.cpp
-//  BDD2
-//
-//  Created by Benjamin Lefebvre on 29/03/13.
-//  Copyright (c) 2013 Benjamin Lefebvre. All rights reserved.
-//
-
-#include "GraphColoring.h"
