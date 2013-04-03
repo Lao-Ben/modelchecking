c6f3db354d0ffa1cf6a148f34be979ff83add3ad
Thibault Lapassade <thibault.lapassade@free.fr>
2013-04-03 19:47:10 +0200

    modification du fichier de coloration du graphe car erreur de merging

--- BDD/src/GraphColoring.cpp
+++ BDD/src/GraphColoring.cpp
@@ -14,6 +14,8 @@
 
 #include "../include/GraphColoring.h"
 
+#include "../include/BDD.h"
+
 #include <stdio.h>
 #include <string.h>
 
@@ -49,20 +51,7 @@ BDD* edge(std::string v1, std::string v2)
 {
   BDD* bdd1;
   std::ostringstream constraint;
-
-//  n'ajouter que si le sommet est rencontré pour la 1e fois
-//  if (std::find(vertexArray.begin(), vertexArray.end(), v1) == vertexArray.end()) {
-//  	constraint << "( " << RED << v1 <<  " xor " << GREEN << v1 <<  " xor " << BLUE << v1 <<  " xor " << YELLOW << v1 <<  " )";
-//  	constraint << " && ";
-//  }
-//  vertexArray.push_back(v1);
-
-//  if (std::find(vertexArray.begin(), vertexArray.end(), v2) == vertexArray.end()) {
-//	  constraint << "( " << RED << v2 <<  " xor " << GREEN << v2 <<  " xor " << BLUE << v2 <<  " xor " << YELLOW << v2 <<  " )";
-//  	constraint << " && ";
-//  }
-//  vertexArray.push_back(v2);
-
+  
   constraint << "( ( "
   << REDGREEN << v1
   << " xor " << REDGREEN << v2 << " ) || ( "
@@ -78,17 +67,14 @@ BDD* edge(std::string v1, std::string v2)
   }
   else
   {
-    bdd1 = new BDD(bdd->getNbVar(), bdd->getVectorNode(), bdd->getVectNode(), bdd->getNodeFalse(), bdd->getNodeTrue(), bdd->getVarorder(), bdd->getOrdervar(), bdd->getMaxIndice(), bdd->getOpmap());
-//    bdd1 = bdd1->andfonc(bdd1, constraint.str());
-//    bdd = bdd->andfonc(bdd, constraint.str());
+    bdd1 = new BDD(bdd->getNbVar(), bdd->getVectVar(), bdd->getVectorNode(), bdd->getVectNode(), bdd->getNodeFalse(), bdd->getNodeTrue(), bdd->getVarorder(), bdd->getOrdervar(), bdd->getMaxIndice(), bdd->getOpmap());
     bdd1->setExpression(constraint.str());
     bdd1->build();
     bdd = bdd->andfonc(bdd,	bdd1);
     bdd = bdd->transferinfo(bdd1, bdd);
   }
-
+  
   std::cout << "build sous bdd : " << v1 << "," << v2 << std::endl;
-//  std::cout << "Nombre de solution satisfaisante pour sous bdd: " << bdd->satcount() << std::endl;
 	return bdd;
 }
 
@@ -97,13 +83,13 @@ BDD* edge(std::string v1, std::string v2)
 void exportGraphColors (std::string solution)
 {
   std::ofstream outFile("graph.data");
-
+  
   std::istringstream iss(solution);
   std::string token;
   while(getline(iss, token, ';'))
   {
     std::string prefix = token.substr(2,2);
-
+    
     if (hasEnding(token, "true"))
     {
       if (hasBegining(token, "[R"))
@@ -145,7 +131,7 @@ void exportGraphColors (std::string solution)
 
   std::map<std::string, int>::const_iterator it;
   std::ostringstream oss;
-
+  
   for (it = colorMap.begin(); it != colorMap.end(); ++it)
   {
     switch ((*it).second) {
@@ -167,7 +153,7 @@ void exportGraphColors (std::string solution)
 
   outFile << oss.str() << std::endl;
 
-
+  
   outFile.close();
 }
 
@@ -180,11 +166,11 @@ int GraphColoring::compute()
   clock_t start, end;
 	double elapsed;
   start = clock();
-
+  
   bdd = new BDD();
   bdd->setNbVar(NBVARS);
-
-
+  
+  
   edge("AL","FL"); edge("AL","GA"); edge("AL","MS"); edge("AL","TN"); // 5
   edge("AR","LA"); edge("AR","MO"); edge("AR","MS"); edge("AR","OK"); // 9
   edge("AR","TN"); edge("AR","TX"); edge("AZ","CA"); edge("AZ","NM"); // 13
@@ -199,34 +185,34 @@ int GraphColoring::compute()
   edge("IL","IN"); edge("IL","KY"); edge("IL","MO"); edge("IL","WI"); // 40
   edge("IN","KY"); edge("IN","MI"); edge("IN","OH"); edge("KS","MO"); // 42
   edge("KS","NE"); edge("KS","OK"); edge("KY","MO"); edge("KY","OH"); // 44
-  edge("KY","TN"); edge("KY","VA"); edge("KY","WV"); edge("LA","MS"); // 46
-  edge("LA","TX"); edge("MA","NH"); edge("MA","NY"); edge("MA","RI"); // 48  // time 274 273      60
+  edge("KY","TN"); edge("KY","VA"); edge("KY","WV"); edge("LA","MS"); // 46                         2.2GHz      3.4GHz
+  edge("LA","TX"); edge("MA","NH"); edge("MA","NY"); edge("MA","RI"); // 48  // time 274 273        60          57
   edge("MA","VT"); edge("MD","PA"); edge("MD","VA"); edge("MD","WV");
   edge("ME","NH"); edge("MI","OH"); edge("MI","WI"); edge("MN","ND");
   edge("MN","SD"); edge("MN","WI"); edge("MO","NE"); edge("MO","OK");
-  edge("MO","TN"); edge("MS","TN"); edge("MT","ND"); edge("MT","SD"); // time : 800s              252s
+  edge("MO","TN"); edge("MS","TN"); edge("MT","ND"); edge("MT","SD"); // time : 800s              252s          231
   edge("MT","WY"); edge("NC","SC"); edge("NC","TN"); edge("NC","VA");
   edge("ND","SD"); edge("NE","SD"); edge("NE","WY"); edge("NH","VT"); //                          574
   edge("NJ","NY"); edge("NJ","PA"); edge("NM","OK"); edge("NM","TX"); //                          870
-//  edge("NV","OR"); edge("NV","UT"); edge("NY","PA"); edge("NY","VT");
-//  edge("OH","PA"); edge("OH","WV"); edge("OK","TX"); edge("OR","WA");
-//  edge("PA","WV"); edge("SD","WY"); edge("TN","VA"); edge("UT","WY");
-//  edge("VA","WV");
-
-
+  edge("NV","OR"); edge("NV","UT"); edge("NY","PA"); edge("NY","VT");
+  edge("OH","PA"); edge("OH","WV"); edge("OK","TX"); edge("OR","WA");
+  edge("PA","WV"); edge("SD","WY"); edge("TN","VA"); edge("UT","WY");
+  edge("VA","WV");
+  
+  
   //std::cout << "Nombre de solution satisfaisante pour bdd: " << bdd->satcount() << std::endl;
-
-
+  
+  
   std::string solution = bdd->anysat();
-
+  
   std::cout << "Une solution : " << solution << std::endl;
-
+  
   // performance analysis
   end = clock();
   elapsed = ((double)end - start) / CLOCKS_PER_SEC;
   std::cout << "Execution time : " << elapsed << std::endl;
-
+  
   exportGraphColors(solution);
-
+  
   return 0;
 }
